0c03ac14987f03f737d21d7fbfa813608173275c
Aaron Goldsmith <aarongoldsmith@msn.com>
2017-08-01 22:30:59 -0700

    add axios, get request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
-const API_KEY = '6a084b8d34522755f7236486f4a97cc2'
+import axios from 'axios';
+
+const API_KEY = '6a084b8d34522755f7236486f4a97cc2';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city}, us`;
+  const request = axios.get(url);
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+
+  };
+
+}
