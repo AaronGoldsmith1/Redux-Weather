9f71a0292763d3e57aa294a1b0edf3f9eedb8284
Aaron Goldsmith <aarongoldsmith@msn.com>
2017-08-02 21:51:31 -0700

    pressure, humidity charts

--- src/components/chart.js
+++ src/components/chart.js
@@ -5,7 +5,7 @@ export default (props) => {
   return (
     <div>
 
-      <Sparklines height={120} width={180} data={props.data}>
+      <Sparklines svgHeight={120} svgWidth={180} data={props.data}>
       <SparklinesLine color={props.color} />
       </Sparklines>
 
