d8e0a27d906edf9a6adcf027401cb95e3c94d85f
Aaron Goldsmith <aarongoldsmith@msn.com>
2017-09-04 00:46:18 -0700

    update weather list

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 
-
 class GoogleMap extends Component {
 
   componentDidMount() {
-    new google.maps.map(this.refs.map, {
+    new google.maps.Map(this.refs.map, {
       zoom: 12,
       center: {
         lat: this.props.lat,
@@ -14,7 +13,6 @@ class GoogleMap extends Component {
   }
 
   render() {
-    //this.refs.map
     return <div ref="map" />;
   }
 }
